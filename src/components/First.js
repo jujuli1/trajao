0dd5e6737895dcbb3a9904f10e5de788f94b1800
LEFEVRE <lefevrejulien@live.fr>
2025-01-10 15:43:56 +0100

    first commit

--- src/components/First.js
+++ src/components/First.js
@@ -8,6 +8,16 @@ const First = () => {
             
             <img src={Fleur} className='photoProfile'/>
             <div className='cadreProfil'></div>
+            <div className='presentation'>
+
+            <h1>JE ME RESENTE</h1>
+            <p>Je suis Tanya RAJAONARIVELO, dessinatrice et peintre.</p>
+            <p>Installée en Bretagne depuis 10 ans et d'origine Malgache, j'expose depuis maintenant 4 ans</p>
+            <p>Je suis membre de deux associations artistiques dans le pays de Saint Brieuc :</p>
+            <p>La ruche des Arts 22 et le collectif La Brioche (créateur des fanzines briochins , BROC) .</p>
+
+            </div>
+            
         </div>
     );
 };
